dcdda36ed8d805921e0d23ef5fa79cc8dc5c7a46
J <joabsonangelo7@ooutlook.com>
2024-01-28 15:24:23 -0300

    operadores e calculo de media

---
+++ aula02_trabalhando_com_Js/exercicio05_Operadores.js
@@ -0,0 +1,19 @@
+const notas = [10.0, 10.0, 10.0]
+
+console.log(`A primeira nota é: ${notas[0]}`);
+console.log(`A segunda notas é: ${notas[1]}`);
+console.log(`Á terceira nota é: ${notas[2]}`);
+
+var media = (notas[0]+notas[1]+notas[2])/notas.length;
+
+console.log(`a media é: ${media}`);
+
+if (media >= 7) {
+    if (media == 10) {
+      console.log("Aprovado com Mérito");
+    } else {
+      console.log("Aprovado");
+    }
+  } else {
+    console.log("Reprovado");
+  }
---
+++ aula02_trabalhando_com_Js/exercio04_ObjLiteral.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome : "Joabson",
+    cpf : "123456789",
+    cidade: "Joao pessoa"
+}
+
+console.log(pessoa.nome);
+console.log(pessoa.cpf);
+console.log(pessoa.cidade); 
