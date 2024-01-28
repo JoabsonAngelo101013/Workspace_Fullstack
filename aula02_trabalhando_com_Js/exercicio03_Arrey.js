e59c84153f3ba1b9ffa804a8a7de02e5bcfc133a
J <joabsonangelo7@ooutlook.com>
2024-01-28 14:16:11 -0300

    exercios de arrey

---
+++ aula02_trabalhando_com_Js/exercicio03_Arrey.js
@@ -0,0 +1,8 @@
+const notas = [8.0, 9.0]
+
+console.log(`A primeira nota é: ${notas[0]}`);
+console.log(`A segunda notas é: ${notas[1]}`);
+
+var media = (notas[0]+notas[1])/notas.length;
+
+console.log(`a media foi: ${media}`);
