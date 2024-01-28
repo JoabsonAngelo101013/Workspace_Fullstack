b3871a77ac31da5188f000f6c7b0a68c0c4ca733
J <joabsonangelo7@ooutlook.com>
2024-01-28 13:57:29 -0300

    exercicio

---
+++ aula02_trabalhando_com_Js/exercicio02.js
@@ -0,0 +1,9 @@
+const tecnologia = "React";
+const tecnologia2 = "JavaScript"
+const tecnologia3 = "Java"
+const tecnologia4 = "Spring"
+function objetivoDoCurso (tecnologia){
+    return `O objetivo do curso é aprender ${tecnologia} e ${tecnologia2} e ${tecnologia3} e ${tecnologia4}`;
+}
+
+console.log(objetivoDoCurso(tecnologia));
