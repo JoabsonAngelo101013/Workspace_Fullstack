a192183d258d20ce6e3cb25b318be8f9766d9ff8
J <joabsonangelo7@ooutlook.com>
2024-02-16 21:55:11 -0300

    exercicio com css e react

---
+++ aula04_Trabalhando_Css_react/treinando_css_aula04/src/App.jsx
@@ -0,0 +1,20 @@
+import Exercicio_Css from "./componente/Exercicio_Css"
+
+
+
+
+const App=() => {
+ 
+
+  return (
+  
+    <div> 
+
+      <Exercicio_Css/>
+      
+      </div>
+    
+  )
+}
+
+export default App
---
+++ aula04_Trabalhando_Css_react/treinando_css_aula04/src/componente/Exercicio_Css.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import img from '../assets/img/foto.png'
+
+
+
+const Exercicio_Css = () => {
+  return (
+    <div>
+      <h1>Exercicio CSS com React</h1>
+      <p>Olá Css Global !!!!!</p>
+      <img src={img} alt= "imagem" />
+    </div>
+  )
+}
+
+export default Exercicio_Css
