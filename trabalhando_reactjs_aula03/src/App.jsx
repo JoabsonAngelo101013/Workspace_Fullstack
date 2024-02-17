0ad4154423115a884f2afb3c783c05251f29b66e
J <joabsonangelo7@ooutlook.com>
2024-02-16 21:09:42 -0300

    revisao

--- trabalhando_reactjs_aula03/src/App.jsx
+++ trabalhando_reactjs_aula03/src/App.jsx
@@ -1,12 +1,27 @@
-import HookMegaSena from "./components/HookMegaSena"
+import Aluno from "./components/Aluno"
 
 function App() {
+  const aluno1 ={
+    nome: "Joabson",
+    email: "Joabsonangelo@gmail.com",
+    curso: "Curso Fullstack"
+  }
+  const aluno2 = {
+    nome: "Maria",
+    email:"Maria@gmail.com",
+    curso:"Curso Fullstack"
+  }
 
+  const alunos = [aluno1, aluno2];
   
   return (
-    <>
-    <HookMegaSena/>
-    </>
+   <div>{
+    alunos.map((aluno) => (
+      <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso}/>
+    ))
+    }
+
+   </div>
   )
 }
 
