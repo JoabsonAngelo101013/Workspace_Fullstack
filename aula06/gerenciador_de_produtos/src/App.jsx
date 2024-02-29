32041e3978b799583eea6785c59e38d56ebf54ea
J <joabsonangelo7@ooutlook.com>
2024-02-29 17:25:55 -0300

    criação de um banco de dados mokado e aplicação de css com bootstrap

--- aula06/gerenciador_de_produtos/src/App.jsx
+++ aula06/gerenciador_de_produtos/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
-import './App.css'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar1 from "./components/Navbar1"
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import SupplierList from "./pages/Supplier/SupplierList";
+import SupplierForm from "./pages/Supplier/SupplierForm";
 
 
 
@@ -14,9 +15,13 @@ function App() {
     <>
      <BrowserRouter>
      <Navbar1/>
-     <div className="container">
-      
-     </div>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<SupplierList />} />
+            <Route path="/add-fornecedor" element={<SupplierForm/>} />
+            <Route path="/listar-fornecedores" element={<SupplierList/>} />
+          </Routes>
+        </div>
      </BrowserRouter>
     </>
   )
--- aula06/gerenciador_de_produtos/src/pages/Supplier/SupplierList.jsx
+++ aula06/gerenciador_de_produtos/src/pages/Supplier/SupplierList.jsx
@@ -1,21 +1,76 @@
 import React from 'react'
 import axios from '../../api'
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const SupplierList = () => {
     const [suppliers, setSuppliers] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios.get('/suppliers')
+            .then(response => {
+                setSuppliers(response.data)
+            })
+            .catch(error => console.error("There was an error!", error))
+    }, []);
+
+const fetchSuppliers = () => {
+    axios.get('/suppliers')
         .then(response => {
             setSuppliers(response.data)
         })
         .catch(error => console.error("There was an error!", error))
-    },[]);
-  return (
-    <div>
-      
+
+}
+
+function deleteSupplier(id) {
+    axios.delete(`/suppliers/${id}`)
+        .then(() => {
+            alert("Fornecedor excluido com sucesso!")
+            fetchSuppliers()
+
+        })
+        .catch(error => console.error("There was an error!", error))
+}
+
+
+
+//importado do bootstrap
+return (
+    <div className='container mt-5'>
+        <h2 className='mb-4'>Lista de Fornecedores</h2>
+        <button onClick={() => navigate('/add-fornecedor')} className='btn btn-primary mb-2'>Adicionar Fornecedor</button>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Cnpj</th>
+                    <th>Email</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    suppliers.map(supplier => (
+                        <tr key={supplier.id}>
+                            <td>{supplier.name}</td>
+                            <td>{supplier.cnpj}</td>
+                            <td>{supplier.email}</td>
+                            <td>
+                                <button className='btn btn-sm btn-warning mr-2'>Editar</button>
+                                <button onClick={() => deleteSupplier(supplier.id)} className='btn btn-sm btn-danger mr-2'>Excuir</button>
+
+                            </td>
+
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+
     </div>
-  )
+)
 }
 
 export default SupplierList
