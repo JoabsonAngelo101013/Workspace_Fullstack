e106c7b06b93b749753c6c7a4331fb571f10abee
J <joabsonangelo7@ooutlook.com>
2024-04-15 19:42:40 -0300

    Criação do securit e endpoints do costumer

---
+++ aula06/gerenciador_de_produtos/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="menu">
+        <div>
+            <Link to="/">Painel de Controle</Link>
+            <Link to="/add-fornecedor">Adicionar Fornecedor</Link>
+            <Link to="/listar-fornecedores">Listar Fornecedores</Link>
+            <Link to="/listar-produtos">Listar Produtos</Link>
+            <Link to="/add-produto">Adicionar Produto</Link>
+            <Link to="/add-cliente">Adicionar Cliente</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
